f646c3945be08c12e932fa3191ab1e9f2626e7d6
AbanoubEmadM <emadbebo729@gmail.com>
2025-11-30 16:08:31 +0200

    Create RoomController and Define Room Resource Route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RoomController;
 
 Auth::routes();
 
 Route::view('/', 'pages.hotelmaster')->name('home');
+Route::middleware('auth')->group(function () {
+    Route::resource('/rooms', RoomController::class);
+});
